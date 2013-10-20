3c57a0c4c185e1fd48c199a2523921a28670bfde
Michael Pirogov <vbnet.ru@gmail.com>
2013-10-20 14:48:02 +0400

    Merged shit

--- lib/rails_datatables.rb
+++ lib/rails_datatables.rb
@@ -1,6 +1,6 @@
 module ActionView
   module Helpers
-	def datatable(columns, opts={})
+    def datatable(columns, opts={})
       sort_by = opts[:sort_by] || nil
       additional_data = opts[:additional_data] || {}
       search = opts[:search].present? ? opts[:search].to_s : "true"
@@ -33,7 +33,7 @@ module ActionView
       server_side = opts[:ajax_source].present?
 
       additional_data_string = ""
-      additional_data.each_pair do |name,value|
+      additional_data.each_pair do |name, value|
         additional_data_string = additional_data_string + ", " if !additional_data_string.blank? && value
         additional_data_string = additional_data_string + "{'name': '#{name}', 'value':'#{value}'}" if value
       end
@@ -73,7 +73,7 @@ module ActionView
               "bFilter": #{search},
               "bAutoWidth": #{auto_width},
               #{"'aaSorting': [#{sort_by}]," if sort_by}
-              #{"'sAjaxSource': '#{ajax_source}'," if ajax_source}
+      #{"'sAjaxSource': '#{ajax_source}'," if ajax_source}
               "aoColumns": [
                   #{formatted_columns(columns)}
               ],
@@ -94,87 +94,28 @@ module ActionView
 
     private
 
-      jqueryui = opts.key?(:jqueryui) ? opts[:jqueryui].to_s : "false"
-      paginate = opts[:paginate].present? ? opts[:pagintate].to_s : "true"
-      surl = opts[:surl] || nil
-      destroy = opts[:destroy].present? ? opts[:destroy].to_s : "false"
-      append = opts[:append] || nil
-      ajax_source = opts[:ajax_source] || nil
-      server_side = opts[:ajax_source].present?
-      jquery_ui = opts[:jquery_ui] || nil
-      dom = opts[:dom] || nil
+    def formatted_columns(columns)
+      i = 0
+      columns.map { |c|
+        i += 1
+        if c.nil? or c.empty?
+          "null"
+        else
+          searchable = c[:searchable].to_s.present? ? c[:searchable].to_s : "true"
+          sortable = c[:sortable].to_s.present? ? c[:sortable].to_s : "true"
 
-      additional_data_string = ""
-      additional_data.each_pair do |name,value|
-        additional_data_string = additional_data_string + ", " if !additional_data_string.blank? && value
-        additional_data_string = additional_data_string + "{'name': '#{name}', 'value':'#{value}'}" if value
-      end
-
-      %Q{
-      <script type="text/javascript">
-      jQuery(document).ready(function () {
-          jQuery('#{table_dom_id}').dataTable({
-              "oLanguage": {
-                  "sSearch": "#{search_label}",
-                  #{"'sUrl': '#{surl}'," if surl}
-                  #{"'sZeroRecords': '#{no_records_message}'," if no_records_message}
-                  "sProcessing": '#{processing}'
-              },
-              "bDestroy": #{destroy},
-              "bPaginate": #{paginate},
-              "sPaginationType": "full_numbers",
-              "iDisplayLength": #{per_page},
-              "bJQueryUI": #{jqueryui},
-              "bProcessing": #{use_processing},
-              "bServerSide": #{server_side},
-              "bLengthChange": false,
-              "bStateSave": #{persist_state},
-              "bFilter": #{search},
-              "bAutoWidth": #{auto_width},
-              #{"'sDom': '#{dom}'," if dom}
-              #{"'aaSorting': [#{sort_by}]," if sort_by}
-              #{"'sAjaxSource': '#{ajax_source}'," if ajax_source}
-              #{"'bJQueryUI': #{jquery_ui}," if jquery_ui}
-              "aoColumns": [
-                  #{formatted_columns(columns)}
-              ],
-              #{"'fnRowCallback': function( nRow, aData, iDisplayIndex ) {
-                  #{row_callback}
-              }," if row_callback}
-              "fnServerData": function ( sSource, aoData, fnCallback ) {
-                  aoData.push( #{additional_data_string} );
-                  jQuery.getJSON( sSource, aoData, function (json) {
-                      fnCallback(json);
-                  });
-              }
-          })#{append};
-      });
-      </script>
-      }.html_safe
-    end
-
-    private
-
-      def formatted_columns(columns)
-        i = 0
-        columns.map {|c|
-          i += 1
-          if c.nil? or c.empty?
-            "null"
-          else
-            searchable = c[:searchable].to_s.present? ? c[:searchable].to_s : "true"
-            sortable = c[:sortable].to_s.present? ? c[:sortable].to_s : "true"
-
-            "{
+          "{
                 'sType': '#{c[:type] || "string"}',
                 'bSortable':#{sortable},
                 'bSearchable':#{searchable}
-            #{"'asSorting':#{c[:sorting].inspect}," if c[:sorting]}
-            #{"'bVisible':'#{c[:visible]}'," if c[:visible]}
-            #{"'iDataSort':#{c[:datasort]}," if c[:datasort]}
-            #{"'sClass':'#{c[:class]}'" if c[:class]}
+          #{"'asSorting':#{c[:sorting].inspect}," if c[:sorting]}
+          #{"'bVisible':'#{c[:visible]}'," if c[:visible]}
+          #{"'iDataSort':#{c[:datasort]}," if c[:datasort]}
+          #{"'sClass':'#{c[:class]}'" if c[:class]}
             }"
-          end
-        }.join(",")
-      end
-  end      
+        end
+      }.join(",")
+    end
+  end
+end
+
